d86c071ea55054c9c276c1534fd4a813da0b2d92
CiroGalanVertiz <152623830+CiroGalanVertiz@users.noreply.github.com>
2026-04-07 18:45:12 +0200

    a

---
+++ TEMA8/Actividad1/Ejercicio4/src/main/java/com/mycompany/ejercicio4/Ejercicio4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio4;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM121
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        System.out.println("Introduce el nombre de una ruta");
+        File file = new File(new Scanner(System.in).next());
+        if(file.exists() && file.isDirectory()){
+            for(File f: file.listFiles()){
+                System.out.println(f.getName());
+            }
+        }
+    }
+}
