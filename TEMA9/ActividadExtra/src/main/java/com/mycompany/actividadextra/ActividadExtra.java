4e1dc274966c8e4d4f8323e87bb48d0610245074
CiroGalanVertiz <152623830+CiroGalanVertiz@users.noreply.github.com>
2026-05-02 18:51:46 +0200

    A

---
+++ TEMA9/ActividadExtra/src/main/java/com/mycompany/actividadextra/ActividadExtra.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividadextra;
+
+import com.mycompany.actividadextra.InterfazGrafica.Pantalla;
+
+/**
+ *
+ * @author cococ
+ */
+public class ActividadExtra {
+
+    public static void main(String[] args) {
+      
+       Pantalla pantalla = new Pantalla();
+       pantalla.setLocationRelativeTo(null);
+       pantalla.setVisible(true);
+       
+    }
+}
---
+++ TEMA9/ActividadExtra/src/main/java/com/mycompany/actividadextra/Persona.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.actividadextra;
+
+/**
+ *
+ * @author cococ
+ */
+public class Persona {
+    private String nombre,profesion,rutaImagen;
+
+    public Persona(String nombre, String profesion, String rutaImagen) {
+        this.nombre = nombre;
+        this.profesion = profesion;
+        this.rutaImagen = rutaImagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+    
+}
